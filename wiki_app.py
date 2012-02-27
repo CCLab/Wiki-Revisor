cdec2253f8d2786307da711a91e9205ac12dfa34
Bartosz Stalewski <bartekstalewski@gmail.com>
2012-02-27 14:48:35 +0100

    Search cached, single-part-wiki-data bug fixed

--- wiki_app.py
+++ wiki_app.py
@@ -28,40 +28,22 @@ def propositions( db ):
     query = request.query.query
     lang = request.query.lang
     
-    if DATA_DEBUG:
-            print 'search_propositions: BEFORE'
-    propositions = search_propositions( query, lang )
-    if DATA_DEBUG:
-            print 'search_propositions: AFTER'
-    if DATA_DEBUG:
-        print 'is_query_cached: BEFORE'
-    is_cached = is_query_cached( db, query, lang )
-    if DATA_DEBUG:
-            print 'is_query_cached: AFTER'
+    if not is_search_data_cached( db, query, lang):
+        update_query_hits( db, query, lang )
+    propositions = get_query_hits( db, query, lang )
+
     return {
         'propositions': propositions,
-        'cached'      : is_cached
+        'cached'      : False
     }
     
 @route('/data')
 def data( db ):
-    cached = ( request.query.cached == 'true' )
     query = request.query.query
     lang = request.query.lang
     
-    if cached:
-        if DATA_DEBUG:
-            print 'get_cached_data: BEFORE'
-        data = get_cached_data( db, query, lang )
-        if DATA_DEBUG:
-            print 'get_cached_data: AFTER'
-    else:
-        if DATA_DEBUG:
-            print 'get_fresh_data: BEFORE'
-        data = get_fresh_data( db, query, lang )
-        if DATA_DEBUG:
-            print 'get_fresh_data: AFTER'
-    
+    data = get_data( db, query, lang )
+        
     return { 'data': data }
 
 
@@ -79,7 +61,12 @@ def server_static(filename):
 @route('/static/js/<filename>')
 def server_static(filename):
     return static_file(filename, root='static/js')
-    
+
+
+#TODO: REMOVE !!!!!
+@route('/purge/')
+def purge_db():
+    init_db( db_path, drop=True )
     
 # create tables if any is missing, if drop is True, then drop all tables
 def init_db( path, drop=False ):
@@ -88,14 +75,18 @@ def init_db( path, drop=False ):
     cursor = conn.cursor()
     
     if drop:
+        print 'Droping tables!'
         cursor.execute('''drop table id_map''')
+        cursor.execute('''drop table search_map''')
         cursor.execute('''drop table search_data''')
         cursor.execute('''drop table editions''')
         cursor.execute('''drop table last_editions''')
     
-    
     create_id_map_query = '''create table if not exists id_map
                           (query text, lang text, query_id integer)
+    '''
+    create_search_map_query = ''' create table if not exists search_map
+                                  (query text, lang text, query_id integer)
     '''
     create_search_data_query = '''create table if not exists search_data
                                   (query_id integer, title text)
@@ -108,11 +99,13 @@ def init_db( path, drop=False ):
     '''
     
     cursor.execute( create_id_map_query )
+    cursor.execute( create_search_map_query )
     cursor.execute( create_search_data_query )
     cursor.execute( create_editions_query )
     cursor.execute( create_last_editions_query )
     
-init_db( db_path )
+init_db( db_path, drop=False )
+#init_db( db_path, drop=True )
 
 # start server
 run(host='localhost', port=8080, debug=True, reloader=True)
@@ -120,8 +113,32 @@ run(host='localhost', port=8080, debug=True, reloader=True)
 
 # DB
 
+def get_query_hits( db, query, lang ):
+    query_id = get_query_id( db, query, lang, table='search_map' )
+    db_query = '''select title from search_data
+                  where query_id = ?'''
+    results = db.execute( db_query, (query_id,) ).fetchall()
+    return results if results is None else [t[0] for t in results]
+
+def update_query_hits( db, query, lang ):
+    query_id = get_or_create_query_id( db, query, lang, table='search_map' )
+
+    propositions = search_propositions( query, lang )
+    insert_query = '''insert into search_data
+                      values (?,?)'''
+    for proposition in propositions:
+        db.execute( insert_query, (query_id, proposition) )
+
+def is_search_data_cached( db, query, lang ):
+    return is_query_cached( db, query, lang, table='search_map' )
+
+def get_data( db, query, lang ):
+    if is_query_cached( db, query, lang ):
+        return get_cached_data( db, query, lang )
+    else:
+        return get_fresh_data( db, query, lang )
+
 def get_cached_data( db, query, lang ):
-    print '3DB:', db
     query_id = get_query_id( db, query, lang )
     
     return {
@@ -130,14 +147,9 @@ def get_cached_data( db, query, lang ):
     }
     
 def get_fresh_data( db, query, lang ):
-    print 'LANG:', lang
-    print 'QUERY:', query
-    print '1DB:', db
-    
     fresh_data = grab_data( lang, query )
     update_full_data( db, query, lang, fresh_data )
     
-    print '2DB:', db
     return get_cached_data( db, query, lang )
     
 def update_full_data( db, query, lang, data ):
@@ -147,30 +159,33 @@ def update_full_data( db, query, lang, data ):
     update_last_edition( db, query_id )
 
     
-def get_query_id( db, query, lang ):
-    db_query = '''select query_id from id_map
+def get_query_id( db, query, lang, table='id_map' ):
+    db_query = '''select query_id from ''' + table + ''' 
                   where query=? and lang=?'''
     query_id = db.execute( db_query, (query, lang) ).fetchone()
     
     return query_id if query_id is None else query_id[0]
     
-def get_or_create_query_id( db, query, lang ):
-    query_id = get_query_id( db, query, lang )
+def get_or_create_query_id( db, query, lang, table='id_map' ):
+    query_id = get_query_id( db, query, lang, table )
     if query_id is None:
-        new_query_id = get_max_query_id( db, query, lang ) + 1
-        db_query = '''insert into id_map
+        new_query_id = get_max_query_id( db, table ) + 1
+        db_query = '''insert into ''' + table + ''' 
                       values (?,?,?)'''
         db.execute( db_query, (query, lang, new_query_id) )
         return new_query_id
     else:
         return query_id
         
-def get_max_query_id( db, query, lang ):
+def get_max_query_id( db, table='id_map' ):
     min_id = 0
-    db_query = '''select query_id from id_map'''
+    db_query = '''select query_id from ''' + table
     ids = db.execute( db_query, () ).fetchall()
-    print 'ids:', ids
-    return max( ids )[0] if len( ids ) > 0 else min_id
+
+    last_max_id = min_id
+    for t in ids:
+        last_max_id = max( last_max_id, t[0] )
+    return last_max_id
     
 def update_editions( db, query_id, editions ):
     # remove old values
@@ -216,20 +231,9 @@ def get_editions( db, query_id ):
     # sort by date
     return sorted( editions, key=lambda t: 12*t['year'] + t['month'] )
     
-# res = (year, month, count)
-# def sort_fun( res1, res2 ):
-    # if res1[0] != res2[0]:
-        # return res1[0] - res2[0]
-    # else:
-        # return res1[1] - res2[1]
-    
-def is_query_cached( db, query, lang ):
-    id = get_query_id( db, query, lang )
+def is_query_cached( db, query, lang, table='id_map' ):
+    id = get_query_id( db, query, lang, table )
     return id is not None
-    
-# TODO: maybe use?
-def update_search_data( db, query, lang, data):
-    pass
     
 # Getting data from wikipedia
 
@@ -239,7 +243,7 @@ def search_propositions( query, lang ):
         'action'   : 'query',
         'format'   : 'json',
         'list'     : 'search',
-        'srsearch' : query
+        'srsearch' : query.encode('utf-8')
     })
 
     # read stringified json result
@@ -269,13 +273,11 @@ def search_propositions( query, lang ):
     
 
 def grab_data( lang, hit ):
-    print 'LANG:', lang
-    print 'HIT:', hit
     host = 'http://'+lang+'.wikipedia.org/w/api.php'
     data = {
         'action'  : 'query',
         'format'  : 'json',
-        'titles'  : hit,
+        'titles'  : hit.encode('utf-8'),
         'prop'    : 'revisions',
         'rvlimit' : 100
     }
@@ -298,17 +300,16 @@ def grab_data( lang, hit ):
         # f.close()
 
     revision_dates = {}
-    done = False
-    while not done:
+    while True:
         for page_id in revision_results['query']['pages']:
             for revision in revision_results['query']['pages'][page_id]['revisions']:
                 d = revision['timestamp'].split('T')[0].rsplit('-',1)[0]
                 revision_dates[ d ] = revision_dates.get( d, 0 ) + 1
 
-        try:
-            next_rev = str(revision_results['query-continue']['revisions']['rvstartid'])
-        except:
-            done = True
+        if 'query-continue' not in revision_results:
+            break
+
+        next_rev = str(revision_results['query-continue']['revisions']['rvstartid'])
 
         # update the results offset
         data.update({ 'rvstartid': next_rev })
